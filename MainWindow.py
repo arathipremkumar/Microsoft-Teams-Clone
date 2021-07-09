6d4b5a4264689a94b0e8044e83f6da5eee40a53f
arathipremkumar <57580997+arathipremkumar@users.noreply.github.com>
2021-07-09 22:24:49 +0530

    Update MainWindow.py

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'MainWindow.ui'
-#
 # Created by: PyQt5 UI code generator 5.15.1
 #
 # WARNING: Any manual changes made to this file will be lost when pyuic5 is
